111c57d58af18e67ba554da4468e4e6f160ef884
Chee Hong Lan <cheehong146@gmail.com>
2021-05-08 02:25:58 +0800

    Initial Commit

---
+++ BoostTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoostTest
+//
+//  Created by Lan Chee Hong on 08/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
